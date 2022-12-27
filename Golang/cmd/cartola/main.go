0d0c9ac798c6ea62a536e16d223a9632bbf9de44
Cassius Thalles Costa Mendes <cassiustcmendes@hotmail.com>
2022-12-27 19:40:04 -0300

    Finish Go App and connect with Kafka

--- Golang/cmd/cartola/main.go
+++ Golang/cmd/cartola/main.go
@@ -4,16 +4,21 @@ import (
 	"context"
 	"database/sql"
 	"net/http"
+
 	"mygolangapp/pkg/uow"
+	httphandler "mygolangapp/internal/infra/http"
 	"mygolangapp/internal/infra/repository"
 	"mygolangapp/internal/infra/db"
+	"mygolangapp/internal/infra/kafka/consumer"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	ctx := context.Background()
-	dtb, err := sql.Open("mysql", "root:root@tcp(mysql:3307)/cartola?parseTime=true")
+	dtb, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/cartola?parseTime=true")
 
 	if err != nil {
 		panic(err)
@@ -25,9 +30,19 @@ func main() {
 	}
 	registerRepositories(uow)
 
-	if err = http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	r := chi.NewRouter()
+	r.Get("/players", httphandler.ListPlayersHandler(ctx, *db.New(dtb)))
+	r.Get("/my-teams/{teamID}/players", httphandler.ListMyTeamPlayersHandler(ctx, *db.New(dtb)))
+	r.Get("/my-teams/{teamID}/balance", httphandler.GetMyTeamBalanceHandler(ctx, *db.New(dtb)))
+	r.Get("/matches", httphandler.ListMatchesHandler(ctx, repository.NewMatchRepository(dtb)))
+	r.Get("/matches/{matchID}", httphandler.ListMatchByIDHandler(ctx, repository.NewMatchRepository(dtb)))
+
+	go http.ListenAndServe(":8080", r)
+
+	var topics = []string{"newMatch", "chooseTeam", "newPlayer", "matchUpdateResult", "newAction"}
+	msgChan := make(chan *kafka.Message)
+	go consumer.Consume(topics, "broker:9094", msgChan)
+	consumer.ProcessEvents(ctx, msgChan, uow)
 }
 
 func registerRepositories(uow *uow.Uow) {
